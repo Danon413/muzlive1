95cc9675d9cf5205ff2ae82cc8b52d9383be90ac
Danon413 <151877429+Danon413@users.noreply.github.com>
2025-05-23 01:57:57 +0300

    Update Program.cs

--- ShopWebsite/src/Client/Program.cs
+++ ShopWebsite/src/Client/Program.cs
@@ -1,48 +1,24 @@
-global using ShopWebsite.Shared.Models.Data;
-global using ShopWebsite.Shared.Models.DataTransferObjects;
-global using System.Net.Http.Json;
-global using ShopWebsite.Client.Services.ProductService;
-global using ShopWebsite.Client.Services.CategoryService;
-global using ShopWebsite.Client.Services.CartService;
-global using ShopWebsite.Client.Services.AuthService;
-global using ShopWebsite.Client.Services.OrderService;
-global using ShopWebsite.Client.Services.AddressService;
-global using ShopWebsite.Client.Services.ProductTypeService;
-global using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using ShopWebsite.Client.Utils.ServiceRegistration;
-using Blazored.LocalStorage;
 using ShopWebsite.Client;
+using ShopWebsite.Client.Services.CartService;
+using ShopWebsite.Client.Services.ProductService;
+using ShopWebsite.Client.Services.ProductTypeService;
+using ShopWebsite.Client.Services.CategoryService;
+using ShopWebsite.Client.Services.AuthService;
+using ShopWebsite.Client.Services.AddressService;
+using ShopWebsite.Client.Services.OrderService;
 
-//Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NCaF5cXmZCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWXlfdXVWQ2ZcUkZzWUU=");
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddShopWebsiteServices();
-builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddOptions();
-builder.Services.AddAuthorizationCore();
-builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
-
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage(); // Показывает детальные ошибки
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-}
-
-app.UseStaticFiles();
-app.UseRouting();
-app.MapRazorPages();
-
-app.Run();
+builder.Services.AddScoped<ICartService, CartService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IProductTypeService, ProductTypeService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IAddressService, AddressService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
 
 await builder.Build().RunAsync();
